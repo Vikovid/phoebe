06355a8e667aa1ac6f82341d06cec32a0b2572af
VikoDev <vikovid.devs@gmail.com>
2023-11-14 16:46:53 -0600

    proyecto phoebe 1

--- index.php
+++ index.php
@@ -1,38 +1,59 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="css/main.css" rel="stylesheet">
-    <title>Login</title>
+    <link rel="stylesheet" href="css/main.css">
+    <title>Phoebe</title>
 </head>
 
 <body>
-    <div class="wrapper">
-        <form action="">
-            <h1>Login</h1>
-            <div class="input-box">
-                <input type="text" placeholder="Usuario" required>
-                <i class='bx bx-user-circle' ></i>
+    <header class="header">
+        <a href="#" class="logo">Phoebe</a>
+        <nav class="navbar">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+            <button class="btnLogin-popup">Ingresar</button>
+        </nav>
+    </header>
+    <section class="section">
+        <div class="wrapper">
+            <div class="logreg-box">
+                <div class="form-box login">
+                    <div class="logreg-title">
+                        <h2>Iniciar Sesión</h2>
+                        <p>Por favor ingrese sus datos</p>
+                    </div>
+                    <form action="#">
+                        <div class="input-box">
+                            <span class="icon"><i class='bx bxs-user'></i></span>
+                            <input type="text" required>
+                            <label>Usuario</label>
+                        </div>
+                        <div class="input-box">
+                            <span class="icon"><i class='bx bxs-lock-alt'></i></span>
+                            <input type="password" required>
+                            <label>Contraseña</label>
+                        </div>
+                        <div class="remember-forgot">
+                            <label><input type="checkbox">Recordar contraseña</label>
+                            <a href="#">¿Olvidaste tu contraseña?</a>
+                        </div>
+                        <button type="submit" class="btn">Iniciar Sesión</button>
+                        <div class="logreg-link">
+                            <p>
+                                ¿Ya tienes una cuenta? <a href="#" class="register-link">Registrarse</a>.
+                            </p>
+                        </div>                        
+                    </form>
+                </div>
             </div>
-            <div class="input-box">
-                <input type="password" placeholder="Contraseña" required>
-                <i class='bx bx-lock' ></i>
-            </div>
-            <div class="remember-forgot">
-                <label><input type="checkbox"></label>
-                <a href="#">¿Olvidó su contraseña?</a>
-            </div>
-
-            <button type="submit" class="btn">Ingresar</button>
-
-            <div class="register-link">
-                <p>¿No posee una cuenta? <a href="#">Crear nueva cuenta</a></p>
-            </div>
-        </form>
-    </div>
-    </div>
+        </div>
+    </section>
 </body>
 </html>
